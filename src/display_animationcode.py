084d2542ec3d163dcb943107c907b440e34e753b
Robin Reyer <mail@robin-reyer.de>
2025-08-10 17:39:59 +0200

    save for the day

--- src/display_animationcode.py
+++ src/display_animationcode.py
@@ -1,3 +1,3 @@
 # init display connection
 # startup animation
-# animate animationcodes once a minute
+# animate animationcodes twice a minute (after executing, calculated  necessary sleep time to stay in the rythm)
