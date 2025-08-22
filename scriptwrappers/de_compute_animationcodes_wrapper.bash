ce3dfb0f1e41d5d6429ff376c7163dfa252246a9
Robin Reyer <mail@robin-reyer.de>
2025-08-22 17:00:13 +0200

    add stretch segments hamburg altona -> hannover hbf

---
+++ scriptwrappers/de_compute_animationcodes_wrapper.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# execute every full minute
+
+
+cd /home/robin/Documents/github/heidelberg-train-monitor/src/de
+
+sudo /home/robin/Documents/github/heidelberg-train-monitor/venv/bin/python ./de_compute_animationcodes.py
--- scriptwrappers/preprocess_static_wrapper.bash
+++ scriptwrappers/preprocess_static_wrapper.bash
@@ -7,3 +7,6 @@ sudo /home/robin/Documents/github/heidelberg-train-monitor/venv/bin/python ./rnv
 
 cd /home/robin/Documents/github/heidelberg-train-monitor/src/db
 sudo /home/robin/Documents/github/heidelberg-train-monitor/venv/bin/python ./db_preprocess_static.py
+
+cd /home/robin/Documents/github/heidelberg-train-monitor/src/de
+sudo /home/robin/Documents/github/heidelberg-train-monitor/venv/bin/python ./de_preprocess_static.py
